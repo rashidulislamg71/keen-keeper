4d0a7833a3ab2cb134c48c6b86ea0c875e28e4db
rashidulislamg71 <jrashidulislam0103@gmail.com>
2026-04-14 10:02:44 +0600

    navbar added

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,20 +2,50 @@ import React from 'react'
 import { CiClock2 } from 'react-icons/ci';
 import { FaHome } from 'react-icons/fa'
 import { TfiStatsUp } from "react-icons/tfi";
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 function NavBar() {
     return (
         <div>
-            <nav>
-                <div children="text-3xl md:text-3xl font-bold">
-                    <span className='text-sky-700'>Keen</span>
-                    <span className='text-sky-700'>Keeper</span>
+            <nav className='flex items-center justify-between px-4 py-2 shadow-2xl md:px-8 md:py-4'>
+                <div className='text-xl font-bold md:text-2xl'>
+                    <Link to={`/`}>
+                        <span className='text-sky-800 md:text-2xl'>Keen</span>
+                        <span className='text-[#244d3f] '>Keeper</span>
+                    </Link>
                 </div>
-                <div>
-                    <NavLink><span><FaHome /></span>Home</NavLink>
-                    <NavLink><span><CiClock2 /></span>Timeline</NavLink>
-                    <NavLink><span><TfiStatsUp /></span>Stats</NavLink>
+                <div className='flex justify-center items-center gap-5 md:gap-10'>
+
+                    <NavLink
+                        to="/"
+                        className={({ isActive }) =>
+                            `flex items-center gap-1 px-3 py-1 rounded 
+            ${isActive ? 'bg-[#244d3f] text-white' : ''}`
+                        }
+                    >
+                        <FaHome /> Home
+                    </NavLink>
+
+                    <NavLink
+                        to="/timeline"
+                        className={({ isActive }) =>
+                            `flex items-center gap-1 px-2 py-1 rounded 
+            ${isActive ? 'bg-[#244d3f] text-white' : ''}`
+                        }
+                    >
+                        <CiClock2 /> Timeline
+                    </NavLink>
+
+                    <NavLink
+                        to="/stats"
+                        className={({ isActive }) =>
+                            `flex items-center gap-1 px-2 py-1 rounded 
+            ${isActive ? 'bg-[#244d3f] text-white' : ''}`
+                        }
+                    >
+                        <TfiStatsUp /> Stats
+                    </NavLink>
+
                 </div>
             </nav>
         </div>
