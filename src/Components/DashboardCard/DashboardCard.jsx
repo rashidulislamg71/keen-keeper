5553a0f2b84206abee870d33b2d8952b3339cad3
rashidulislamg71 <jrashidulislam0103@gmail.com>
2026-04-16 07:37:30 +0600

    fixed responsive

--- src/Components/DashboardCard/DashboardCard.jsx
+++ src/Components/DashboardCard/DashboardCard.jsx
@@ -5,7 +5,7 @@ function DashboardCard({ number, title }) {
         <div>
             <div className='bg-white shadow rounded text-center py-5 md:py-8'>
                 <h3 className='text-2xl text-gray-600 font-bold'>{number}</h3>
-                <p className='text-gray-400 text-sm '>{title}</p>
+                <p className='text-gray-400 text-sm md:text-base'>{title}</p>
             </div>
         </div>
     )
--- src/Components/NavBar/LinkComponents.jsx
+++ src/Components/NavBar/LinkComponents.jsx
@@ -6,7 +6,7 @@ function LinkComponent({ path, children }) {
         <div>
             <NavLink to={path} className={({ isActive }) =>
                 `flex items-center md:gap-1 md:px-2 py-1 rounded 
-            ${isActive ? 'text-[#244d3f] font-bold md:font-normal md:bg-[#244d3f] md:text-white' : ''}`
+            ${isActive ? 'text-[#244d3f]  md:bg-[#244d3f] md:text-white' : ''}`
             } > {children} </NavLink>
         </div>
     )
